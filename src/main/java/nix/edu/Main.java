5aa99e033b8200991c4444d932a785b9ac10b019
sabina30r <sabina3005.15@gmail.com>
2020-07-14 01:03:08 +0300

    home task 10 already done

---
+++ src/main/java/nix/edu/Main.java
@@ -0,0 +1,65 @@
+package nix.edu;
+
+import nix.edu.service.StreamService;
+
+import java.time.LocalDate;
+import java.time.Year;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.UnaryOperator;
+
+
+public class Main {
+
+    static long numberOfGeneratedNumeric = 50;
+
+    static double[] doubles = {1.0, 5.0, 7.0, 5.5, 8.0, 9.5, 2.0, 3.0, 7.8, 8.2};
+
+    static List<Year> listOfYears = Arrays.asList(Year.of(2007), Year.of(2000), Year.of(2017), Year.of(2010),
+            Year.of(2004), Year.of(2019), Year.of(2008), Year.of(2012), Year.of(2015), Year.of(2016), Year.of(2020));
+
+    static List<String> stringsRegexExtract = Arrays.asList("some3 string5", "some547 string5", "1so32me string8");
+
+    static List<String> stringsForPalindromes = Arrays.asList("Was it a car    or a cat I saw ",
+            "    Some men interpret    nine memos", "Life is a journey", "Pull up if    I pull up", "Now or never");
+
+    static List<LocalDate> birthdays = Arrays.asList(LocalDate.of(1995, 12, 1),
+            LocalDate.of(1999, 12, 15),
+            LocalDate.of(2000, 05, 30),
+            LocalDate.of(1996, 12, 20),
+            LocalDate.of(1998, 04, 03),
+            LocalDate.of(1993, 04, 23),
+            LocalDate.of(2000, 03, 25),
+            LocalDate.of(1989, 04, 06));
+
+    static int[] evenAndOddArray = {5, 7, 1, 9, 12, 4, 6, 18, 22, 16, 81, 2, 17};
+
+    static List<UnaryOperator<String>> unaryOperatorList = Arrays.asList(s -> s.toUpperCase(),
+            s -> "[" + s + "]",
+            s -> s + s);
+
+    static String stringForUnary = "java";
+
+    public static void main(String[] args) {
+        System.out.println("-------------- TASK 1 --------------");
+        StreamService.findArrayMaxElement(doubles);
+        StreamService.findArrayMinElement(doubles);
+        StreamService.findArrayAverage(doubles);
+        StreamService.findArraySum(doubles);
+        System.out.println("-------------- TASK 2 --------------");
+        StreamService.findLeapYears(listOfYears);
+        System.out.println("-------------- TASK 3 --------------");
+        StreamService.extractSubstringByRegex(stringsRegexExtract);
+        System.out.println("-------------- TASK 4 --------------");
+        StreamService.isPalindromeInList(stringsForPalindromes);
+        StreamService.returnIfPalindrome(stringsForPalindromes);
+        System.out.println("-------------- TASK 5 --------------");
+        StreamService.generatePrimeSequence(numberOfGeneratedNumeric);
+        System.out.println("-------------- TASK 6 --------------");
+        StreamService.countNumberOfBirthPerMonth(birthdays);
+        System.out.println("-------------- TASK 7 --------------");
+        StreamService.splitArrayIntoEvenAndOdd(evenAndOddArray);
+        System.out.println("-------------- TASK 8 --------------");
+        StreamService.applyAllUnaryOperators(unaryOperatorList, stringForUnary);
+    }
+}
---
+++ src/main/java/nix/edu/service/StreamService.java
@@ -0,0 +1,128 @@
+package nix.edu.service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.OptionalDouble;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+
+public class StreamService {
+
+    public static void findArrayMaxElement(double[] array) {
+        DoubleStream stream = DoubleStream.of(array);
+        OptionalDouble maxElement = stream.max();
+        if (maxElement.isPresent()) {
+            System.out.println("Array max element = " + maxElement.getAsDouble());
+        } else {
+            System.out.println("No value");
+        }
+    }
+
+    public static void findArrayMinElement(double[] array) {
+        DoubleStream stream = DoubleStream.of(array);
+        OptionalDouble minElement = stream.min();
+        if (minElement.isPresent()) {
+            System.out.println("Array min element = " + minElement.getAsDouble());
+        } else {
+            System.out.println("No value");
+        }
+    }
+
+    public static void findArrayAverage(double[] array) {
+        DoubleStream stream = DoubleStream.of(array);
+        OptionalDouble average = stream.average();
+        if (average.isPresent()) {
+            System.out.println("Array average = " + average.getAsDouble());
+        } else {
+            System.out.println("No value");
+        }
+    }
+
+    public static void findArraySum(double[] array) {
+        DoubleStream stream = DoubleStream.of(array);
+        double sum = stream.sum();
+        System.out.println("Array sum = " + sum);
+    }
+
+    public static void findLeapYears(List<Year> list) {
+        List<Year> leapYears = list.stream()
+                .filter(i -> i.isLeap())
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println("Leap years: " + leapYears);
+    }
+
+    public static void extractSubstringByRegex(List<String> list) {
+        List<String> strings = list.stream()
+                .map((s) -> s.replaceAll("[0-9]", ""))
+                .collect(Collectors.toList());
+        System.out.println(strings);
+    }
+
+    public static void isPalindromeInList(List<String> list) {
+        List<String> strings = list.stream()
+                .map((s) -> s.replaceAll("\\s+", "")
+                        .toLowerCase()).collect(Collectors.toList());
+        List<Boolean> isPalindromesList = strings.stream().map((s) -> IntStream.range(0, s.length() / 2)
+                .noneMatch(i -> s.charAt(i) != s.charAt(s.length() - i - 1))).collect(Collectors.toList());
+        System.out.println("Check if strings in list are palindromes: " + isPalindromesList);
+
+    }
+
+    public static boolean isPalindrome(String str) {
+        String tempString = str.replaceAll("\\s+", "").toLowerCase();
+        return IntStream.range(0, tempString.length() / 2)
+                .noneMatch(i -> tempString.charAt(i) != tempString.charAt(tempString.length() - i - 1));
+    }
+
+    public static void returnIfPalindrome(List<String> list) {
+        List<String> palindromes = list.stream()
+                .filter(s -> isPalindrome(s) == true)
+                .collect(Collectors.toList());
+        System.out.println("Palindromes: " + palindromes);
+    }
+
+    public static boolean isPrime(long N) {
+        return LongStream.rangeClosed(2, (long) Math.sqrt(N))
+                .allMatch(i -> N % i != 0);
+    }
+
+    public static void generatePrimeSequence(long N) {
+        List<Long> primes = LongStream.iterate(2, i -> i + 1)
+                .filter(i -> isPrime(i))
+                .limit(N)
+                .boxed()
+                .collect(Collectors.toList());
+        System.out.println("Prime numbers: " + primes);
+    }
+
+    public static void countNumberOfBirthPerMonth(List<LocalDate> list) {
+        Map<Month, Integer> mapOfBirthdaysPerMonth = list.stream()
+                .map(i -> i.getMonth())
+                .collect(Collectors.groupingBy(i -> i, Collectors.reducing(0, e -> 1, Integer::sum)));
+        System.out.println("Number of birthdays in each month: " + mapOfBirthdaysPerMonth);
+    }
+
+    public static void splitArrayIntoEvenAndOdd(int[] array) {
+        int[] evenNumbers = Arrays.stream(array)
+                .filter(i -> i % 2 == 0).sorted().toArray();
+        int[] oddNumbers = Arrays.stream(array)
+                .filter(i -> i % 2 != 0).sorted().toArray();
+        System.out.println("Even numbers: " + Arrays.toString(evenNumbers));
+        System.out.println("Odd numbers: " + Arrays.toString(oddNumbers));
+    }
+
+    public static void applyAllUnaryOperators(List<UnaryOperator<String>> list, String str) {
+        UnaryOperator<String> combinedOperator = list.stream()
+                .reduce(s -> s, (a, b) -> s -> b.apply(a.apply(s)));
+        System.out.println("Combine applying of operators: " + combinedOperator.apply(str));
+    }
+}
